4040859865f18e74c2da82412ffb9e3716bcb34a
xubinbin1024 <596928539@qq.com>
2017-07-12 14:32:44 +0800

    tofu继承广告 SDK :: fire

---
+++ app/src/main/java/com/dragon/hyper/SplashActivity.java
@@ -0,0 +1,72 @@
+package com.dragon.hyper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.sdk.core.InitSDK;
+import com.sdk.core.ad.callback.OnAdManagerCallback;
+import com.sdk.core.ad.callback.OnShowAdDetailCallback;
+import com.sdk.core.ad.exception.AdException;
+import com.sdk.core.ad.splash.manager.AdManagerSplash;
+
+import java.io.IOException;
+
+import static com.sdk.core.ad.common.Config.SPLASH_POS;
+
+public class SplashActivity extends AppCompatActivity implements OnAdManagerCallback,OnShowAdDetailCallback{
+
+    private RelativeLayout mRelativeLayout;//广告位
+    private AdManagerSplash mAdManagerSplash;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.ad_container);
+        try {
+            InitSDK.init(this);
+        } catch (AdException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            mAdManagerSplash = AdManagerSplash.getManagerSplash().setOnAdManagerCallback(this).checkLocalAd(SPLASH_POS);
+        } catch (AdException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onAdExist() {
+        try {
+            mAdManagerSplash.loadAd(mRelativeLayout).setOnShowAdDetailCallback(this);
+        } catch (AdException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onAdNotExist(String s) {
+        startActivity(new Intent(this,MainActivity.class));
+
+        finish();
+    }
+
+    @Override
+    public void onShowAdDetail(WebView webView) {
+        Toast.makeText(this, "没做跳转", Toast.LENGTH_SHORT).show();
+        Log.i(this.getClass().getSimpleName(),"跳转链接 ：" + webView.getUrl());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdManagerSplash.release();
+    }
+}
